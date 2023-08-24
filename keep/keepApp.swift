0a221b68a8eba2447dad34dad6e40fb536d41011
ThangTQ <tothang97nbvn@gmail.com>
2023-08-24 14:03:20 +0700

    Initial Commit

---
+++ keep/keepApp.swift
@@ -0,0 +1,17 @@
+//
+//  keepApp.swift
+//  keep
+//
+//  Created by ThangTQ on 24/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct keepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
